a41aab6f9065bf3eff3cab49b6cbe61632d65eed
Nils Schweinsberg <mail@nils.cc>
2013-10-08 02:56:08 +0200

    Hello-World server

--- server/src/Server.hs
+++ server/src/Server.hs
@@ -1,4 +1,15 @@
-module Main where
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Control.Monad
+import qualified Control.Exception as E
+import Network
+import System.IO
 
 main :: IO ()
-main = print "Hello World!"
+main = withSocketsDo $ do
+  s <- listenOn (PortNumber 9678)
+  forever $ E.handle (\(_ :: E.IOException) -> return ()) $ do
+    (h,_,_) <- accept s
+    l <- hGetLine h
+    hPutStrLn h $ "Hi Kyle! You wrote: \"" ++ l ++ "\""
+    hClose h
