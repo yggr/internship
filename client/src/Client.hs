b5d951fd333819984796ba272fc46f903b50cda0
yggr <zhyostka@gmail.com>
2013-10-12 02:57:14 -0600

    JSON functionality

--- client/src/Client.hs
+++ client/src/Client.hs
@@ -1,7 +1,13 @@
-module Main where
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 
 import Network
 import System.IO
+import Data.Aeson
+import Data.Text
+import Control.Applicative
+import Control.Monad
+import GHC.Generics
+import qualified Data.ByteString.Lazy as B
 
 host :: HostName
 host = "nils.cc"
@@ -9,18 +15,36 @@ host = "nils.cc"
 port :: PortID 
 port = (PortNumber 9678)
 
+data User = 
+  User  { firstName ::  !Text
+        , lastName  ::  !Text
+        , age       ::  Int
+        , sn        ::  !Text
+          } deriving (Show,Generic)
+
+instance FromJSON User
+instance ToJSON User
+
+data Report = 
+  Report  { original_message  ::  !Text
+          , success           ::  Bool
+            } deriving (Show, Generic)
+
+instance FromJSON Report
+instance ToJSON Report
+
 main :: IO ()
 main = withSocketsDo $ do
-  --connect to the server
   h <- connectTo host port 
   hSetBuffering h LineBuffering
+  hSetBinaryMode h True
   
-  --send a message
-  hPutStrLn h "Heisann!"
+  let s = User "Nils" "Schweinsberg" 99 "McManiaC"
+  B.hPut h (encode s)
+  B.hPut h "\n"
+  putStrLn "Data sent"
 
-  --receive a message
-  m <- hGetLine h
-  print m
-
-  --close the handle
-  hClose h
+  d <- eitherDecode <$> (B.hGetContents h) :: IO (Either String Object) 
+  case d of
+    Left err -> putStrLn err
+    Right ps -> print ps
